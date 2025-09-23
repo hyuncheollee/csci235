d5139b284f2ed4b9341194293cc6b7b17756ff1f
hyuncheol <hyleesk17@gmail.com>
2025-09-23 02:41:27 -0400

    Add files via upload
    this is needed for task B

---
+++ projects/project01/File.cpp
@@ -0,0 +1,10 @@
+/*
+Hyuncheol Lee
+2025/09/20
+
+this file 
+*/
+
+//filename_ // a string containing the filename of the File object
+//readable_ // a boolean containing the `read` permission of the File
+// writeable // a boolean containing the `write` permission of the File
